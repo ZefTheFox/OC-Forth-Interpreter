2ed7ab0342550ba049400a7f9fccb37d6dc8c7b9
ZefTheFox <44857466+ZefTheFox@users.noreply.github.com>
2019-04-05 22:27:45 -0400

    Added TICKS and partial implementation of IF

--- forth.lua
+++ forth.lua
@@ -4,6 +4,35 @@ local text = require("text")
 local component = require("component")
 local interpret
 
+local function tableLen(tab); 
+  local count = 0 
+  for _ in pairs(tab) do;
+    count = count + 1; 
+  end; 
+  return count; 
+end
+
+local function gatherStringUntil(searchTerm, splitInput, wordIndex)
+  local returnString = ""
+  local End = 0
+  local hasEnd = false
+  for i = wordIndex+1, tableLen(splitInput) do
+    if string.find(splitInput[i], searchTerm) then; hasEnd = true; End = i; break; end
+  end
+
+  for i = wordIndex+1, End do
+    preparedInput = splitInput[i]
+    if preparedInput == searchTerm then
+      preparedInput = ""
+    elseif string.find(preparedInput, searchTerm) then
+      preparedInput = string.sub(preparedInput, 1, string.find(preparedInput, searchTerm)-string.len(searchTerm))
+    end
+    returnString = returnString.." "..preparedInput
+  end
+
+  return returnString, End, hasEnd
+end
+
 -- Current Word List and additional information
 local WORDS = {
   WORDS = {isLua = true}, ["."] = {isLua = true},
@@ -12,12 +41,15 @@ local WORDS = {
   HOE = {isLua = true}, PAGE = {isLua = true},
   IOXINIT = {isLua = true}, IOXSET = {isLua = true},
   DUP = {isLua = true}, [".S"] = {isLua = true},
-  [".*"] = {isLua = true},
+  [".*"] = {isLua = true}, ["IOX@"] = {isLua = true},
+  VARIABLE = {isLua = true}, [".V"] = {isLua = true},
+  ["DISKNAME*"] = {isLua = true}, TICKS = {isLua = true},
+  DISKINIT = {isLua = true}, ["="] = {isLua = true},
 }
 
 local STACK = {n = 0} -- Stack, n is number of items in the stack
-local VARS = {}
-local VER = "0.0.1a" -- Version
+VARS = {}
+local VER = "0.0.2a" -- Version
 local HOE = true -- H.O.E. Halt on error
 local IGNORECAPS = false -- When enabled capitalizes inputs automatically
 local IDW = false -- Is defining word, best way I could think of doing this
@@ -26,32 +58,141 @@ local CWN = ""    -- Current Word Name, SHOULD BE BLANK WHEN IDW IS FALSE
 local REDSTONECARD -- Redstone Card, call IOXINIT to search for one
 local SKIPLINES = 0 -- Set SKIPNEXT to true then increase this to skip more lines
 
+-- If statement (equal)
+WORDS["="][1] = function(NEXTWORD, wordIndex)
+  if STACK.n > 1 then
+    if NEXTWORD and string.upper(NEXTWORD) == "IF" then
+      -- Is start of if statement
+      local toRun, End, hasEnd = gatherStringUntil("ELSE", splitInput, wordIndex+1)
+      if STACK[STACK.n] == STACK[STACK.n-1] then
+        -- Statement is true
+        
+        if not hasEnd then
+          toRun, End, hasEnd = gatherStringUntil("THEN", splitInput, wordIndex+1) 
+        else
+          toRun, End, hasEnd = gatherStringUntil("THEN", splitInput, wordIndex+End)
+        end
+        interpret(toRun)
+        SKIPNEXT = true
+        SKIPLINES = End - wordIndex - 1
+      else
+        -- Statement is false
+        --[[if hasEnd then
+          toRun, End, hasEnd = gatherStringUntil("THEN", text.tokenize(gatherStringUntil("ELSE", splitInput, wordIndex+1), wordIndex + End)
+        else
+           
+        end --]]
+        toRun, End, hasEnd = gatherStringUntil("THEN", splitInput, wordIndex+1)
+        SKIPNEXT = true
+        SKIPLINES = End - wordIndex - 1
+      end
+    else
+      -- Not part of an if statement
+      if STACK[STACK.n] == STACK[STACK.n-1] then
+        -- Statement is true
+        STACK[STACK.n] = nil
+        STACK.n = STACK.n-1
+        STACK[STACK.n] = 1
+      else
+        -- Statement is false
+        STACK[STACK.n] = nil
+        STACK.n = STACK.n-1
+        STACK[STACK.n] = 0
+      end
+    end
+  else
+    io.stderr:write("ERROR: Not enough items in stack\n")
+    return 1
+  end
+end
+
+-- Initialize a floppy disk
+WORDS.DISKINIT[1] = function()
+  if pcall(component.proxy(component.list("drive")())) then
+    io.stderr:write("ERROR: No disk inserted\n")
+    return 1
+  else
+    drive = component.proxy(component.list("drive")())
+    return 0
+  end
+end
+
+-- Rename the floppy disk
+WORDS["DISKNAME*"][1] = function(_, wordIndex)
+  local hasEnd = false
+  local End
+  local returnString = ""
+  for i = wordIndex+1, tableLen(splitInput) do
+    if string.find(splitInput[i], "*") then; hasEnd = true; End = i; break; end
+  end
+  if hasEnd then
+    SKIPNEXT = true; SKIPLINES = End - wordIndex - 1
+    local returnString = gatherStringUntil("*", splitInput, wordIndex)
+    if pcall(drive.setLabel(returnString)) then
+      io.stderr:write("ERROR: No disk initialized\n")
+    end
+  else
+    io.stderr:write("ERROR: String doesn't end\n")
+    return 1
+  end
+end
+
+-- Delay by a number of ticks 
+WORDS.TICKS[1] = function()
+  if STACK.n > 0 then
+    local delay = STACK[STACK.n]*0.05
+    STACK.n = STACK.n-1
+    local startTime = os.clock()
+    while startTime + delay > os.clock() do
+      
+    end
+  else
+    io.stderr:write("ERROR: Stack empty\n")
+    return 1
+  end
+end
+
+-- Initialize a variable
+WORDS.VARIABLE[1] = function(NEXTWORD)
+  if NEXTWORD then
+    VARS[NEXTWORD] = 0
+    return 2
+  else
+    io.stderr:write("ERROR: Name needed\n")
+    return 1
+  end
+end
+
+-- Displays all variables
+WORDS[".V"][1] = function()
+  for i, number in pairs(VARS) do
+    io.write(i.." "..number.." / ")
+  end
+  io.write("\n")
+end
+
+-- Reads redstone input and puts it on the stack
+WORDS["IOX@"][1] = function()
+  if REDSTONECARD and STACK.n > 0 then
+    STACK[STACK.n] = REDSTONECARD.getInput(STACK[STACK.n])
+  else
+    io.stderr:write("ERROR: RS Card not Init or Stack Empty\n")
+    return 1
+  end
+end
+
 -- Write text to the screen
 WORDS[".*"][1] = function(_, wordIndex)
   local hasEnd = false
   local End
   local returnString = ""
-  local function tableLen(tab); 
-    local count = 0 
-    for _ in pairs(tab) do;
-      count = count + 1; 
-    end; 
-    return count; 
-  end;
-
   for i = wordIndex+1, tableLen(splitInput) do
     if string.find(splitInput[i], "*") then; hasEnd = true; End = i; break; end
   end
 
   if hasEnd then
-    SKIPNEXT = true; SKIPLINES = End - wordIndex
-    for i = wordIndex+1, End do
-      preparedInput = splitInput[i]
-      if string.find(preparedInput, "*") then
-        preparedInput = string.sub(preparedInput, 1, string.find(preparedInput, "*")-1)
-      end
-      returnString = returnString.." "..preparedInput
-    end
+    SKIPNEXT = true; SKIPLINES = End - wordIndex - 1
+    local returnString = gatherStringUntil("*", splitInput, wordIndex)
     io.write(returnString.."\n")
   else
     io.stderr:write("ERROR: String doesn't end\n")
@@ -195,8 +336,29 @@ WORDS.EXIT[1] = function()
   os.exit(0)
 end
 
+
+-- Manage a variable
+local function manageVar(NEXTWORD, VAR)
+  if NEXTWORD == "!" then
+    if STACK.n > 0 then
+      VARS[VAR] = STACK[STACK.n]
+      STACK[STACK.n] = nil
+      STACK.n = STACK.n-1
+      return 0
+    else
+      return 2
+    end
+  elseif NEXTWORD == "@" then
+    STACK.n = STACK.n+1
+    STACK[STACK.n] = VARS[VAR]
+    return 0
+  else
+    return 1
+  end
+end
+
 -- Interpret The Input
-local function interpret(input)
+function interpret(input)
   splitInput = text.tokenize(input)
 
   for wordIndex, wordText in pairs(splitInput) do
@@ -221,12 +383,22 @@ local function interpret(input)
 
         else
           -- Function is implemented in Forth
-          interpret(WORDS[wordText][1], wordIndex)
+          interpret(WORDS[wordText][1])
         end
       else
         -- The Word Does Not Exist
-        
-        io.stderr:write("ERROR: Word Does Not Exist\n")
+        if VARS[wordText] then
+          local EXITCODE = manageVar(splitInput[wordIndex+1], wordText)
+          if EXITCODE == 0 then 
+            SKIPNEXT = true
+          elseif EXITCODE == 1 then
+            io.stderr:write("ERROR: Variable requires a command\n")
+          else
+            io.stderr:write("ERROR: Stack empty\n")
+          end
+        else
+          io.stderr:write("ERROR: Word Does Not Exist\n")
+        end
       end
   
     end
